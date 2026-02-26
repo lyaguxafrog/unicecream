722114fee30c40453b24727b81ca315bfd6b6a8e
Adrian Makridenko <adrianmakridenko@duck.com>
2026-02-26 14:40:34 +0300

    [~] Version

--- src/unicecream/cli/parser.py
+++ src/unicecream/cli/parser.py
@@ -33,7 +33,7 @@ def build_parser():
     parser.add_argument(
         "--version",
         action="version",
-        version="unicecream 0.2.0",
+        version="unicecream 0.2.1",
     )
 
     parser.add_argument(
